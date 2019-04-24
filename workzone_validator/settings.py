04b5fab6ab69fdd7062cc632c111abcaa8e1c8ea
Amy Mok <amy.mok@gsa.gov>
2019-04-23 18:01:16 -0700

    Remove data-ingest

--- workzone_validator/settings.py
+++ workzone_validator/settings.py
@@ -115,9 +115,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-DATA_INGEST = {
-    'VALIDATORS': {
-        'schema.json': 'data_ingest.ingestors.JsonschemaValidator',
-    },
-}
